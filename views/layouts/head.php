bc8ef3e4355885151a2fbe892b4a9baec466e452
henry-jacq <henryeditz267@gmail.com>
2024-06-18 13:55:17 +0530

    new: UI Components added

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -3,3 +3,18 @@
 <title><?= $this->title ?></title>
 <link rel="stylesheet" href="<?= get_assets('resources/css/app.css') ?>">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style>
+    /* Custom scrollbar styles */
+    ::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: rgba(0, 0, 0, 0.2);
+        border-radius: 8px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: rgba(0, 0, 0, 0.3);
+    }
+</style>
